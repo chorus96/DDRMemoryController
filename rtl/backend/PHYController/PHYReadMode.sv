40eabb1f36dcae57230a8e343cfb956139313738
chorus96 <40328697+chorus96@users.noreply.github.com>
2026-03-17 21:31:24 +0900

    Update PHYReadMode.sv

--- rtl/backend/PHYController/PHYReadMode.sv
+++ rtl/backend/PHYController/PHYReadMode.sv
@@ -149,37 +149,36 @@ module PHYReadMode #(
     parameter int PHYFIFODEPTH      = 32,
     parameter int PHYFIFOMAXENTRY   = 4,
     parameter int BURST_LENGTH      = 8
-)(
-                                                                //             Read Mode (PHY)               //
+) (
     input logic clk, rst, clk2x,                          
-                                                                //          INPUT FROM  DRAM-side             //
-    input logic dqs_t, dqs_c,                                   //  1. Diff. signals for DQ BUS               //
-    input logic [MEM_DATAWIDTH-1:0] inData,                     //  2. Data from DQ-BUS WHEN Read Process     //
+    /* INPUT FROM  DRAM-side */
+    input logic dqs_t, dqs_c,                   // 1. Diff. signals for DQ BUS
+    input logic [MEM_DATAWIDTH - 1 : 0] inData, // 2. Data from DQ-BUS WHEN Read Process
 
-                                                                //          INPUT FROM PHYCONTROLLER          //
-    input logic inflag,                                         //  1. Valid for Receiving Data from DQ-BUS   //
-    input logic outflag,                                        //  2. Valid for Sending Data to Read Buffer  //
+    /* INPUT FROM PHYCONTROLLER */
+    input logic inflag,  // 1. Valid for Receiving Data from DQ-BUS
+    input logic outflag, // 2. Valid for Sending Data to Read Buffer
 
-                                                                //          OUTPUT TO PHYCONTROLLER           //
-    output logic readDataACK,                                   //  1. Read Data ACK WHEN PHY RECEIVES DATA   //
+    /* OUTPUT TO PHYCONTROLLER */
+    output logic readDataACK, // 1. Read Data ACK WHEN PHY RECEIVES DATA 
 
-                                                                //           OUTPUT TO PHYCONTROLLER          //
-    output logic [MEM_DATAWIDTH-1:0] outData,                   //  1. Data to Read Buffer                    //
-    output logic outDataValid,                                  //  2. Valid signal for Read Buffer           //
-    output logic outDataLast                                    //  3. Last signal for Read Buffer            //
+    /* OUTPUT TO PHYCONTROLLER */
+    output logic [MEM_DATAWIDTH - 1 : 0] outData, // 1. Data to Read Buffer                    
+    output logic outDataValid,                    // 2. Valid signal for Read Buffer           
+    output logic outDataLast                      // 3. Last signal for Read Buffer            
 );
 
-    logic [$clog2(PHYFIFODEPTH)-1:0] burst_cnt_dram;            //  Burst Counter for PHY <- DRAM         //
-    logic [$clog2(PHYFIFODEPTH)-1:0] burst_cnt_host;            //  Burst Counter for PHY -> READ BUFFER  //
-    logic [MEM_DATAWIDTH-1:0] readModeFIFO [PHYFIFODEPTH-1:0];  //  Burst Data FIFO   in PHY              //
+    logic [$clog2(PHYFIFODEPTH) - 1 : 0] burst_cnt_dram;               //  Burst Counter for PHY <- DRAM        
+    logic [$clog2(PHYFIFODEPTH) - 1 : 0] burst_cnt_host;               //  Burst Counter for PHY -> READ BUFFER 
+    logic [MEM_DATAWIDTH - 1 : 0] readModeFIFO [PHYFIFODEPTH - 1 : 0]; //  Burst Data FIFO in PHY              
 
     //----  Burst Counter Setup for DQ-BUS Data (Counter) ------//
     always_ff @(posedge clk2x or negedge rst) begin : FIFOPUSHCounter
         if (!rst) begin
             burst_cnt_dram <= '0;
         end else begin
-            if (inflag) begin                            // Inflag is triggered by PHYController
-                if(burst_cnt_dram == PHYFIFODEPTH-1)begin
+            if (inflag) begin // Inflag is triggered by PHYController
+                if(burst_cnt_dram == PHYFIFODEPTH - 1)begin
                     burst_cnt_dram <= 0;
                 end else begin
                     burst_cnt_dram <= burst_cnt_dram + 1;
@@ -188,16 +187,16 @@ module PHYReadMode #(
         end
     end : FIFOPUSHCounter
 
-    always@(posedge clk or negedge rst) begin : InflightReadInDataCounter
-        if(!rst) begin
+    always @(posedge clk or negedge rst) begin : InflightReadInDataCounter
+        if (!rst) begin
             readDataACK <= 0;
         end else begin
-            if(inflag) begin
-                if(burst_cnt_dram[2:0] == (BURST_LENGTH -1))begin
+            if (inflag) begin
+                if (burst_cnt_dram[2 : 0] == (BURST_LENGTH - 1)) begin
                     readDataACK <= 1;
-                    `ifdef DISPLAY
-                        $display("[%0t] PHYReadMode | READ DATA RECEIVED FROM DRAM", $time);
-                    `endif
+        `ifdef DISPLAY
+                    $display("[%0t] PHYReadMode | READ DATA RECEIVED FROM DRAM", $time);
+        `endif
                 end else begin
                     readDataACK <= 0;
                 end
@@ -208,7 +207,7 @@ module PHYReadMode #(
     end : InflightReadInDataCounter
 
     /////////////////////////////////////////////////////////////
-    //--------  Burst InData Setup for DQ-BUS Data  (PUSH) ------//
+    //--------  Burst InData Setup for DQ-BUS Data (PUSH) ------//
     always_ff @(posedge clk2x or negedge rst) begin : FIFOPush
        if(!rst) begin
             for(int i = 0; i < PHYFIFODEPTH; i++) begin
@@ -217,21 +216,21 @@ module PHYReadMode #(
        end  else begin
             if (inflag) begin
                 readModeFIFO[burst_cnt_dram] <= inData;     
-                `ifdef DISPLAY
-                    $display("[%0t] PHYReadMode | RECEIVED READ DATA : %h | Bead: %d", $time, inData, burst_cnt_dram[$clog2(BURST_LENGTH)-1:0]);
-                `endif
+    `ifdef DISPLAY
+                $display("[%0t] PHYReadMode | RECEIVED READ DATA : %h | Bead: %d", $time, inData, burst_cnt_dram[$clog2(BURST_LENGTH)-1:0]);
+    `endif
             end
        end
     end : FIFOPush
     /////////////////////////////////////////////////////////////
 
     //----  Burst Counter Setup for Read Buffer (Counter) -----//
-    always_ff@(posedge clk or negedge rst) begin : FIFOPOPCounter
+    always_ff @(posedge clk or negedge rst) begin : FIFOPOPCounter
         if(!rst) begin
             burst_cnt_host <= 0;
         end else begin
             if(outDataValid) begin
-                if (burst_cnt_host == PHYFIFODEPTH-1) begin
+                if (burst_cnt_host == PHYFIFODEPTH - 1) begin
                     burst_cnt_host <= 0;
                 end else begin
                     burst_cnt_host <= burst_cnt_host + 1;
@@ -242,7 +241,7 @@ module PHYReadMode #(
     /////////////////////////////////////////////////////////////
 
     assign outDataValid = outflag;
-    assign outDataLast  = (burst_cnt_host[2:0] == BURST_LENGTH-1) ? 1 : 0;
+    assign outDataLast  = (burst_cnt_host[2 : 0] == BURST_LENGTH - 1) ? 1 : 0;
     assign outData      = (outflag) ? readModeFIFO[burst_cnt_host] : 0; 
     /////////////////////////////////////////////////////////////
 
